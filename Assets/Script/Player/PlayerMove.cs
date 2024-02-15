f02466174f315084632d3eb213bdb007c9a5b75d
Yusuf <yusuf_zeki1234@hotmail.com>
2024-02-15 10:52:56 +0300

    player shoot added

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -14,11 +14,19 @@ public class PlayerMove : NetworkBehaviour
     public GameObject playerSprite;
     public Text playerName;
     
+    public float rotationSpeed = 5f;
+    public Transform weaponPivot;
+    public Transform bulletPos;
+    public GameObject bullet;
+    
     private SteamPlayerController _steamPlayerController;
+    private Rigidbody2D _rb;
+    private float x, y;
     private void Start()
     {
         playerSprite.SetActive(false);
         _steamPlayerController = GetComponent<SteamPlayerController>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -29,12 +37,38 @@ public class PlayerMove : NetworkBehaviour
              
             playerName.text = _steamPlayerController.PlayerName;
         }
+        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
 
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        Vector3 direction = mousePosition - weaponPivot.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion desiredRotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        weaponPivot.rotation = Quaternion.Lerp(weaponPivot.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Quaternion bulletRotation = bulletPos.rotation;
+
+            GameObject _bullet = Instantiate(bullet, bulletPos.position, bulletRotation);
+
+            Vector2 bulletDirection = bulletRotation * Vector2.right;
+            _bullet.GetComponent<Rigidbody2D>().AddForce(bulletDirection * 1000);
+
+            Destroy(_bullet, 5f);
+        }
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
         CmdMove(x,y);
     }
-    
+
     [Command]
     private void CmdMove(float x, float y)
     {
@@ -45,7 +79,6 @@ public class PlayerMove : NetworkBehaviour
     private void RpcMove(float x, float y)
     {
         Vector3 direction = new Vector3(x,y);
-
-        transform.Translate(direction * speed * Time.deltaTime);
+        _rb.velocity = direction * speed * Time.deltaTime;
     }
 }
